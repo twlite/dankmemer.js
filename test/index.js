ad46e7f26e7b1e2489777ace5766d9dbfc7365a9
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-01-18 13:34:00 +0545

    Create index.js

---
+++ test/index.js
@@ -0,0 +1,22 @@
+const Dank = require("../index");
+const memer = new Dank("Dank Memer Api Key");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log(`Memer is online!`);
+});
+
+client.on("message", async(message) => {
+    if (!message.guild || message.author.bot) return;
+    if (message.content === "spank") {
+        let userone = message.author.displayAvatarURL;
+        let usertwo = client.user.displayAvatarURL;
+        let image = await memer.spank(userone, usertwo);
+
+        const attachment = new Discord.Attachment(image, "spank.png")
+        return message.channel.send(attachment);
+    }
+})
+
+client.login("Discord Bot Token");
