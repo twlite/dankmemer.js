0d07f8c3835d2d41b12738a9a26e44f46a3eac3a
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-01-18 13:33:19 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const dank = require('./lib/Dank');
+
+function Dank(token) {
+    return new dank(token);
+}
+
+Dank.Memer = dank;
+Dank.version = require('./package.json').version;
+
+module.exports = Dank;
